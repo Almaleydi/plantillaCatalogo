811322a395a723adbfe33e97843967ec872a4785
Almaleydi <ing.p.g.almaleydi@gmail.com>
2017-03-01 19:56:09 -0600

    catalogoss

--- conexion.php
+++ conexion.php
@@ -7,7 +7,7 @@ $BD="servcios_libres";*/
 
 $servidor="mysql.hostinger.mx";
 $usuario="u812388712_alma";
-$pwd="periañez";
+$pwd="leydimovil";
 $BD="u812388712_alma";
 
 $conn=mysqli_connect($servidor,$usuario,$pwd,$BD)or die("Error al conectarse a la base de datos".mysqli_error());
