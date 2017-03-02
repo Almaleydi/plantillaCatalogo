2b51d965cb9b5edca552f8e6eddef3dfe6c861c7
Almaleydi <ing.p.g.almaleydi@gmail.com>
2017-03-01 20:14:38 -0600

    recata

--- conexion.php
+++ conexion.php
@@ -6,9 +6,9 @@ $pwd="alex";
 $BD="servcios_libres";*/
 
 $servidor="mysql.hostinger.mx";
-$usuario="u812388712_alma";
-$pwd="leydimovil";
-$BD="u812388712_alma";
+$usuario="u812388716_alma";
+$pwd="periañez";
+$BD="u812388716_alma";
 
 $conn=mysqli_connect($servidor,$usuario,$pwd,$BD)or die("Error al conectarse a la base de datos".mysqli_error());
 ?>
