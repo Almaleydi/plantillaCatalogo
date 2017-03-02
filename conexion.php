a5c5e825878386957ca11d2786986b884e90e173
Almaleydi <ing.p.g.almaleydi@gmail.com>
2017-03-01 20:24:38 -0600

    restablecer

--- conexion.php
+++ conexion.php
@@ -8,7 +8,7 @@ $BD="servcios_libres";*/
 $servidor="mysql.hostinger.mx";
 $usuario="u812388716_alma";
 $pwd="periañez";
-$BD="u812388716_alma";
+$BD="u812388716_bdcat";
 
 $conn=mysqli_connect($servidor,$usuario,$pwd,$BD)or die("Error al conectarse a la base de datos".mysqli_error());
 ?>
